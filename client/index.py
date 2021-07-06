a25d985845efe3261725da883633bdb259f6fbcc
Raphael Antunes Baruffi <42071700+Baruffi@users.noreply.github.com>
2021-07-06 18:38:23 -0300

    extendida funcionalidade do client para permitir cenarios mais diversos com o docker-compose

--- client/index.py
+++ client/index.py
@@ -12,20 +12,25 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = os.environ['SERVER_HOST'] if 'SERVER_HOST' in os.environ else socket.gethostname()
 PORT = int(os.environ['SERVER_PORT']
            ) if 'SERVER_PORT' in os.environ else config['port']
+TOTAL_MESSAGES = int(os.environ['TOTAL_MESSAGES']
+                     ) if 'TOTAL_MESSAGES' in os.environ else config['total_messages']
 
 s.connect((HOST, PORT))
 
 print(f'Conectado socket do cliente na porta: {PORT}')
 
-# s.recv(1024)
+if 'FREEZE_EARLY' in os.environ:
+    s.recv(1024)
 
-while ...:
+for i in range(TOTAL_MESSAGES):
     for i in range(100):
         new_string = bytes(f'test {i}\n', encoding='utf-8')
         s.send(new_string)
 
+    print('Sleeping...')
     time.sleep(5)
 
-# s.recv(1024)
+if 'FREEZE_LATE' in os.environ:
+    s.recv(1024)
 
 s.close()
